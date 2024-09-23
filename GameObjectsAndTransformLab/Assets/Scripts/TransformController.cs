e94418fe8c17f12c2cc66183f52725ec4253e0d6
Connor O'Hara <10959550@uvu.edu>
2024-09-22 22:31:20 -0600

    Completed Scripting Challenge 2
    Changed the rotation and movement of the object

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -8,10 +8,11 @@ public class TransformController : MonoBehaviour
     {
         //Move the target GameObject
         var x = Mathf.PingPong(Time.time, 3);
-        var p = new Vector3(0, x, 0);
+        var y = Mathf.PingPong(Time.time, 6);
+        var p = new Vector3(x, y, 0);
         transform.position = p;
         
         //Rotate the target GameObject
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(15, 30, 10) * Time.deltaTime);
     }
 }
